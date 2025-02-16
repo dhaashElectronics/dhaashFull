56bc7d3b411d43aebbc0b737c1375366c4d8a211
Eng-soke <cabdicasiismaxamed273@gmail.com>
2025-02-16 14:48:45 +0300

    waxaan ku soo daray update

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -67,6 +67,8 @@ const registerUser = async (req, res)=>{
         const salt = await bcrypt.genSalt(10)
 
         const hashedPassword =  await bcrypt.hash(password,salt)
+       
+        
 
         const newUser = new userModel({
             name,
--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -55,22 +55,25 @@ function Orders({ token }) {
 
 
     return (
-        <div>
+        <div className='ml-[32px]'>
             <h3>Order page</h3>
             <div>
 
 
                 {orders.map((order, index) => (
-                    <div className='py-4 border-t border-b text-gray-700 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
+                    <div className=' grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border-2 border-gray-200 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700' key={index}>
                        
                        
-
 
                         <div key={index}>
                             {order.items.map((item, i) => (
-                                <p className='py-0.5 ' key={i}>
+                                <div key={i}> 
+                                      <img className='w-16' src={item.image} />
+                                <p className='py-0.5 ' >
                                     {item.name} x {item.quantity}
                                 </p>
+                                </div>
+                              
                             ))}
 
 
@@ -88,7 +91,7 @@ function Orders({ token }) {
                             <p className='text-sm mb-3 sm:text-[15px]  '>Items : {order.items.length} </p>
                             <p className='mb-3'>Method : {order.paymentMethod}  </p>
                             <p>Payment : {order.payment ? 'Done' : 'Pending'} </p>
-                            <p>Date : {new Date(order.date).toLocaleDateString()} </p>
+                            <p className='mt-2'>Date : {new Date(order.date).toLocaleDateString()} </p>
                         </div>
                         <p className='text-sm sm:text-[15px]'> {currency} {order.amount} </p>
                         <select onChange={(event) => statusHandler(event, order._id)} value={order.status} className='p-2 font-semibold'>
