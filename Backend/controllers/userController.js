6ec922b0718fe3e1efc150244bf779e8e47cf64e
Eng-soke <cabdicasiismaxamed273@gmail.com>
2025-03-04 14:10:38 +0300

    update

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -117,5 +117,4 @@ const adminLogin = async (req, res)=>{
 
 
 
-
 export {adminLogin, loginUser, registerUser}
--- Backend/middleware/adminAuth.js
+++ Backend/middleware/adminAuth.js
@@ -1,18 +1,21 @@
 import jwt from "jsonwebtoken"
 
-const adminAuth = async (req, res,next) =>{
+const adminAuth = async (req, res, next) =>{
     try {
         const {token} = req.headers
+
         if(!token){
             return res.json({success:false, message:"Not Authorized Login Again"})
         }
-        const token_decode = jwt.verify(token,process.env.JWT_SECRET)
+        
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
 
         if(token_decode !== process.env.ADMIN_NAME + process.env.ADMIN_PASSWORD){
             return res.json({success:false, message:"Not Authorized Login Again"})
 
         }
-        next()
+
+        next();
     } catch (error) {
         console.log(error);
         res.json({success:false, message: error.message})
